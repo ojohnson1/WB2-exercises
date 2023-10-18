e6a8b8ef51b4f74489bec51a30376c518ad6ce9d
ojohnson1 <ojohnson659@gmail.com>
2023-10-18 06:48:21 -0400

    added rule of 72 script and functions

---
+++ Mathscripts/rule_of_72.js
@@ -0,0 +1,10 @@
+var savingsAmt=5000.00
+var interestRate= .04
+var doubleAmt= savingsAmt*2
+console.log (72/interestRate)
+
+
+
+console.log('At a ' + interestRate*100 + ' percent interest rate, your savings account will be worth ' + doubleAmt.toFixed (2) + " in " + 72/interestRate/100 + " years " );
+
+
