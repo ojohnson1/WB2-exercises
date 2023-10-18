23e43f464e01e8583c1f50ba8127160d4e0e75f6
ojohnson1 <ojohnson659@gmail.com>
2023-10-18 06:46:50 -0400

    added tip.js and functions

---
+++ tips.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var foodCost = 79.25;
+
+var tax = 6.54;
+
+var tip = 12.00;
+
+var totalDue = foodCost + tax + tip;
+
+console.log("the total due is: " + totalDue);
+
+console.log("food cost is " + foodCost + "and the tax is " + tax);
+
+    console.log("tip is " + tip);
+    console.log("total due is " + totalDue);
+    console.log ('Tip is' + tip.toFixed (2));
