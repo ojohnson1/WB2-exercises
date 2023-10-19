d049ec42e8282bc12275366b139faec4ef586c89
ojohnson1 <ojohnson659@gmail.com>
2023-10-19 14:30:04 -0400

    added functions

---
+++ Ifscripts/greeting.js
@@ -0,0 +1,25 @@
+let currentHour = 14;
+let message ;
+if  (currentHour <= 10 )
+{
+
+message= "Good Morning";
+
+}
+
+
+else if (currentHour >= 11 && currentHour < 17 )
+{
+  message = " Good Afternoon";
+}
+
+
+    else if (currentHour >= 17 )
+    
+    {
+
+    message= "Good Evening";
+       
+    }
+
+    console.log(message)
